b2adf81905fa0b4253c0b167f86d713bdad80369
Pascal Hürten <pascal.huerten@th-luebeck.de>
2025-01-25 19:27:49 +0100

    fix

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,10 +1,10 @@
 extends Node3D
 
 # LevelOne res://scenes/levelOne.tscn
-# Character res://scenes/character.tscn Called "Character" in the current scene
+# Player res://scenes/player.tscn Called "Player" in the current scene
 
 var level_one_scene = preload("res://scenes/levelOne.tscn")
-var character
+var player
 var camera
 var current_level
 var levels = [] # Array to keep track of spawned levels
@@ -19,8 +19,9 @@ func _ready():
 	else:
 		print("OpenXR not initialized, please check if your headset is connected")
 	
-	character = $Character # Assuming the character node is a direct child
-	camera = character.get_node("XRCamera3D") # Assuming the camera is a child of the character
+	player = $Player # Assuming the player node is a direct child
+	print("Player node:", player)
+	camera = player.get_node("XRCamera3D") # Assuming the camera is a child of the player
 	environment = get_node(world_environment_path).environment
 	
 	spawn_level()
@@ -32,24 +33,27 @@ func spawn_level():
 	if !current_level:
 		current_level = level_one_scene.instantiate()
 		add_child(current_level)
-	current_level.global_transform.origin = character.global_transform.origin
+	current_level.global_transform.origin = player.global_transform.origin
 	levels.append(current_level)
 
 func _process(_delta):
-	if character and current_level:
+	if player and current_level:
 		update_current_level()
-		var distance = character.global_transform.origin.distance_to(current_level.global_transform.origin)
+		
+		print("player.global_transform.origin:", player.global_transform.origin)
+		print("current_level.global_transform.origin:", current_level.global_transform.origin)
+		var distance = player.global_transform.origin.distance_to(current_level.global_transform.origin)
 		# Update the level_size based on the dimensions
 		var level_size = current_level.radius + max_distance
 		if distance > level_size:
 			# Rotate camera towards the center of the level
-			character.look_at(current_level.global_transform.origin, Vector3.UP)
+			player.look_at(current_level.global_transform.origin, Vector3.UP)
 			camera.look_at(current_level.global_transform.origin, Vector3.UP)
-			# Move character towards the center of the level by a small amount
+			# Move player towards the center of the level by a small amount
 			var direction = -camera.global_transform.basis.z.normalized()
-			# Move the character towards the center of the level jsut so they are inside the level
+			# Move the player towards the center of the level jsut so they are inside the level
 			var move_distance = distance - level_size + 0.1
-			character.global_transform.origin += direction * move_distance
+			player.global_transform.origin += direction * move_distance
 		
 		# Adjust fog density and ambient light intensity based on distance
 		# var threshold_distance = level_size * 0.5
@@ -61,22 +65,22 @@ func _process(_delta):
 		adjust_environment(factor)
 
 func adjust_environment(factor):
-		# var light_energy = clamp(0.1 - (0.1 * 1 - factor), 0.0, 0.1)
-		var fog_depth_begin_min = 0
-		var fog_depth_begin_max = 12.0
-		var fog_depth_begin = fog_depth_begin_min + (fog_depth_begin_max - fog_depth_begin_min) * factor
+	# var light_energy = clamp(0.1 - (0.1 * 1 - factor), 0.0, 0.1)
+	var fog_depth_begin_min = 0
+	var fog_depth_begin_max = 12.0
+	var fog_depth_begin = fog_depth_begin_min + (fog_depth_begin_max - fog_depth_begin_min) * factor
 
-		var brightness = clamp(1.0 - factor, 0.0, 1.0)
+	var brightness = clamp(1.0 - factor, 0.0, 1.0)
 
-		environment.adjustment_brightness = brightness
-		environment.fog_depth_begin = fog_depth_begin
+	environment.adjustment_brightness = brightness
+	environment.fog_depth_begin = fog_depth_begin
 
 
 func update_current_level():
 	var closest_level = null
 	var closest_distance = INF
 	for level in levels:
-		var distance = character.global_transform.origin.distance_to(level.global_transform.origin)
+		var distance = player.global_transform.origin.distance_to(level.global_transform.origin)
 		if distance < closest_distance:
 			closest_distance = distance
 			closest_level = level
