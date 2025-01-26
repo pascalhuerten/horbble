76dcf017c99ca8d9e140c08a3144045022ed7ff7
Pascal Hürten <pascal.huerten@th-luebeck.de>
2025-01-26 11:03:56 +0100

    Make bubbles float upwards and spaw in tree

--- scripts/level_one.gd
+++ scripts/level_one.gd
@@ -3,32 +3,39 @@ extends Node3D
 # Exported property to set the size in the editor
 @export var radius: int = 10
 
+# Bubble Scene is a RigidBody that can be picked up by the user 
+var bubble_scene = preload("res://scenes/bubble.tscn")
+
+var bubble_spawn
+
+# List to keep track of spawned bubbles
+var bubbles = []
+
+# Maximum distance a bubble can travel before being deleted
+@export var max_distance: float = 300.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Connect the collision signals
-	var bubble = get_node("Bubble")
-	var drop_off = get_node("DropOff")
-	print(bubble, drop_off)
-	if bubble and drop_off:
-		print("Bubble and DropOff nodes found")
-		bubble.connect("body_entered", Callable(self, "_on_bubble_body_entered"))
-		drop_off.connect("body_entered", Callable(self, "_on_drop_off_body_entered"))
-
-# Signal handler for when the bubble collides with another body
-func _on_bubble_body_entered(body: Node) -> void:
-	if body.name == "DropOff":
-		print("Bubble collided with DropOff")
-		_change_lights_color(Color(0, 1, 0)) # Green hue
-		_disable_bubble_grab()
-		_move_bubble_to_hover_target()
-
-# Signal handler for when the drop off collides with another body
-func _on_drop_off_body_entered(body: Node) -> void:
-	if body.name == "Bubble":
-		print("DropOff collided with Bubble")
-		_change_lights_color(Color(0, 1, 0)) # Green hue
-		_disable_bubble_grab()
-		_move_bubble_to_hover_target()
+	bubble_spawn = get_node("BubbleSpawn")
+	
+	# Create and start the spawn timer
+	var spawn_timer = Timer.new()
+	spawn_timer.wait_time = 4.0
+	spawn_timer.one_shot = false
+	spawn_timer.connect("timeout", _spawn_bubble)
+	# Call it once to start the spawning
+	_spawn_bubble()
+	add_child(spawn_timer)
+	spawn_timer.start()
+	
+	# Create and start the check timer
+	var check_timer = Timer.new()
+	check_timer.wait_time = 1.0
+	check_timer.one_shot = false
+	check_timer.connect("timeout", _check_bubble_positions)
+	add_child(check_timer)
+	check_timer.start()
 
 # Function to change the color of all lights in the scene
 func _change_lights_color(color: Color) -> void:
@@ -37,20 +44,22 @@ func _change_lights_color(color: Color) -> void:
 		if light is Light3D:
 			light.light_color = color
 
-# Function to disable the bubble's grab ability
-func _disable_bubble_grab() -> void:
-	var bubble = get_node("Bubble")
-	if bubble:
-		var collision_shape = bubble.get_node("CollisionShape3D")
-		if collision_shape:
-			collision_shape.disabled = true
+# Function to spawn a new bubble at the spawn node
+func _spawn_bubble() -> void:
+	if bubble_scene and bubble_spawn:
+		var new_bubble = bubble_scene.instantiate()
+		bubble_spawn.add_child(new_bubble)
+		new_bubble.global_transform.origin = bubble_spawn.global_transform.origin
+		bubbles.append(new_bubble)
 
-# Function to move the bubble to the hover target position and disable physics
-func _move_bubble_to_hover_target() -> void:
-	var bubble = get_node("Bubble")
-	var hover_target = get_node("HoverTarget")
-	if bubble and hover_target:
-		bubble.global_transform.origin = hover_target.global_transform.origin
-		bubble.freeze = true
-		var laughAudio = hover_target.get_node("Laugh")
-		laughAudio.playing = true
+# Function to check the positions of all bubbles and delete those that go far outside
+func _check_bubble_positions() -> void:
+	var keep_bubbles = []
+	for bubble in bubbles:
+		if bubble.global_transform.origin.distance_to(bubble_spawn.global_transform.origin) > max_distance:
+			bubble.queue_free()
+			print("Bubble deleted")
+		else:
+			keep_bubbles.append(bubble)
+
+	bubbles = keep_bubbles
